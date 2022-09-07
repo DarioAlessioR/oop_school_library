fa5b872dd70c8714d23e22a56ed2946063e4d574
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-09-07 19:36:26 -0400

    Fix linters errors 4

--- person.rb
+++ person.rb
@@ -8,7 +8,7 @@ class Person < Nameable
   def initialize(
     age,
     name = 'Unknown',
-    parent_permission = true
+    parent_permission: true
   )
     super()
     @id = Random.rand(1..1000)
