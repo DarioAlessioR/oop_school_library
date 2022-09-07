11322c73041ad772c0e5fed1cd108f414668c64f
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-09-07 18:33:18 -0400

    Fix linters errors 3

--- main.rb
+++ main.rb
@@ -7,44 +7,44 @@ require_relative './classroom'
 require_relative './rental'
 require_relative './book'
 
-#person = Person.new(22, 'maximilianus')
-#person.correct_name
-#capitalized_person = CapitalizeDecorator.new(person)
-#capitalized_person.correct_name
-#capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-#capitalized_trimmed_person.correct_name
+# person = Person.new(22, 'maximilianus')
+# person.correct_name
+# capitalized_person = CapitalizeDecorator.new(person)
+# capitalized_person.correct_name
+# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+# capitalized_trimmed_person.correct_name
 
-#pp = Person.new(22, 'pepe')
-#puts pp.age
+# pp = Person.new(22, 'pepe')
+# puts pp.age
 
 def main
-    salon = Classroom.new('Salon A')
-    p salon
+  salon = Classroom.new('Salon A')
+  p salon
 
-    salon_b = Classroom.new('Salon B')
-    p salon_b
+  salon_b = Classroom.new('Salon B')
+  p salon_b
 
-    salon_c = Classroom.new('Salon C')
-    p salon_c
+  salon_c = Classroom.new('Salon C')
+  p salon_c
 
-    estud = Student.new(10, salon, 'Jose')
-    p estud
-    
-    salon_b.add_student(estud)
-    p salon_b
-    p estud
+  estud = Student.new(10, salon, 'Jose')
+  p estud
 
-    profe = Teacher.new(55, 'Math', 'Loco')
-    p profe
+  salon_b.add_student(estud)
+  p salon_b
+  p estud
 
-    libro = Book.new('Sapiens', 'Yuval Harari')
-    puts libro.title
-    puts libro.author
+  profe = Teacher.new(55, 'Math', 'Loco')
+  p profe
 
-    renta = Rental.new(2022-01-01, libro, estud)
-    puts renta.book.title
-    puts renta.person.name
-    puts renta.date
+  libro = Book.new('Sapiens', 'Yuval Harari')
+  puts libro.title
+  puts libro.author
+
+  renta = Rental.new('2022-01-01', libro, estud)
+  puts renta.book.title
+  puts renta.person.name
+  puts renta.date
 end
 
 main
