cf93f1c6dd256141e20259fcd02081e3f944d97a
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-09-07 12:48:11 -0400

    Initial commit. Add files rental and book. Update file person

--- student.rb
+++ student.rb
@@ -11,4 +11,9 @@ class Student < Person
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  def belongs_to_classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
 end
