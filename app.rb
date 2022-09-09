22ff37a70418bdfef374b6fe6b1eb44328ea0b25
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-09-08 22:11:55 -0400

    Fix linters errors 9

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require_relative './teacher'
 require_relative './classroom'
 require_relative './rental'
 require_relative './book'
+require_relative './menu'
 
 class App
   def initialize
@@ -13,19 +14,7 @@ class App
   end
 
   def run
-    puts "\n" * 2
-    puts 'Welcome to OOP School Library App!!'
-    puts "\n"
-    puts 'Please, enter a number from the following list to continue:'
-    puts "\n"
-    puts '1) List all books'
-    puts '2) List all people'
-    puts '3) Create a person (teacher or student)'
-    puts '4) Create a book'
-    puts '5) Create a rental'
-    puts '6) List all rentals for a given person id'
-    puts '7) Exit from app'
-    puts "\n"
+    menu
     options
   end
 
---
+++ menu.rb
@@ -0,0 +1,15 @@
+def menu
+  puts "\n" * 2
+  puts 'Welcome to OOP School Library App!!'
+  puts "\n"
+  puts 'Please, enter a number from the following list to continue:'
+  puts "\n"
+  puts '1) List all books'
+  puts '2) List all people'
+  puts '3) Create a person (teacher or student)'
+  puts '4) Create a book'
+  puts '5) Create a rental'
+  puts '6) List all rentals for a given person id'
+  puts '7) Exit from app'
+  puts "\n"
+end
